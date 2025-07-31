1559b6577848c55cbf2b73c0d0cc6e7e1313e4e6
eamus-ruskato <eardizzonemus@gmail.com>
2025-07-31 23:26:04 +0200

    soundbank aggiornata3107

--- ProjectInsect/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ ProjectInsect/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,9 +11,60 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENT_ENERGYFIELD = 3983137402U;
+        static const AkUniqueID AMBIENT_MAINSCENARIO = 263695675U;
+        static const AkUniqueID MUSIC_MAINSCENARIO = 1419702340U;
+        static const AkUniqueID PLAYER_CAUTIONALARM = 4240862283U;
+        static const AkUniqueID PLAYER_MOVEMENT = 541470702U;
+        static const AkUniqueID UI_BUTTON_BACK = 1598004116U;
+        static const AkUniqueID UI_BUTTON_CLICK = 1664509575U;
+        static const AkUniqueID UI_BUTTON_DENIED = 1098067136U;
+        static const AkUniqueID UI_BUTTON_STARTGAME = 916601097U;
+        static const AkUniqueID UI_MAINTENANCE_CLOSE = 1676561250U;
+        static const AkUniqueID UI_MAINTENANCE_OPEN = 701705398U;
+        static const AkUniqueID UI_MAINTENANCE_POWERUP = 120049226U;
+        static const AkUniqueID UI_MAINTENANCE_REPAIR = 26115229U;
+        static const AkUniqueID UI_MISSION_TARGETPOPUP = 2991794566U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PLAYERHEALTHSWITCH
+        {
+            static const AkUniqueID GROUP = 206614296U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PLAYERHEALTH20 = 1703797114U;
+                static const AkUniqueID PLAYERHEALTH50 = 1619908955U;
+                static const AkUniqueID PLAYERHEALTH100 = 3528909237U;
+            } // namespace SWITCH
+        } // namespace PLAYERHEALTHSWITCH
+
+        namespace SURFACETYPE
+        {
+            static const AkUniqueID GROUP = 63790334U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID FOLIAGE = 1162469934U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace SWITCH
+        } // namespace SURFACETYPE
+
+    } // namespace SWITCHES
+
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID CAMERADISTANCE = 2835450985U;
+        static const AkUniqueID MENUATTENUATION = 466717960U;
+        static const AkUniqueID PLAYERHEALTHVALUE = 2774557527U;
+        static const AkUniqueID PLAYERMOVEMENTSPEED = 2557281390U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -25,6 +76,7 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID WORLD = 2609808943U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
