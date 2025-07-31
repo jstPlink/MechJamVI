6861fd65fd5310b3943b1c2dc71d6c8ca3b379c2
jstPlink <fp.dignazio@gmail.com>
2025-07-31 21:30:06 +0200

    aggiornamento macchina stati scorpione e import

--- Assets/Script/Camera_Lerp/look.cs
+++ Assets/Script/Camera_Lerp/look.cs
@@ -39,22 +39,12 @@ public class look : MonoBehaviour
         float yAngle = containerLookAxis * horizontalRotationSpeed * Time.deltaTime;
         float xAngle = cameraLookAxis * verticalRotationSpeed * Time.deltaTime;
         float x = playerCamera.eulerAngles.x + xAngle;
-        //Debug.Log("Angle Normalized X : " + Mathf.Abs(normalizeRotation(x)));
-        //Debug.Log("xAngle : " + xAngle);
-        //Debug.Log("Player Camera X : " + playerCamera.eulerAngles.x);
+
         transform.RotateAround(target.position, Vector3.up, yAngle);
         if (Mathf.Abs(normalizeRotation(x)) < xDegree)
         {
             playerCamera.RotateAround(target.position, playerCamera.right, xAngle);
         }
-        
-        
-        //playerCamera.RotateAround(transform.position, cameraLookAxis, horizontalRotationSpeed * Time.deltaTime);
-
-
-
-
-
     }
 
 
@@ -64,7 +54,6 @@ public class look : MonoBehaviour
         if (y > 180)
         {
             y -= 360;
-            //Debug.Log(y + ": NORMALIZED");
         }
         return y;
     }
