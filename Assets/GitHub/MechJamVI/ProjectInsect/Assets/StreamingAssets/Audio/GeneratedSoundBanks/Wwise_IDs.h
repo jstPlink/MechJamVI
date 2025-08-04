1f8e66ee9bb27ee4e77c7b1816a11e2e1f3bc276
eamus-ruskato <eardizzonemus@gmail.com>
2025-08-04 13:22:42 +0200

    collegamento Musica (vedi descrizione)
    +ambiente
    +movimento player
    +energyfield beacon

---
+++ Assets/GitHub/MechJamVI/ProjectInsect/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,97 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENT_ENERGYFIELD = 3983137402U;
+        static const AkUniqueID AMBIENT_MAINSCENARIO = 263695675U;
+        static const AkUniqueID MUSIC_MAINSCENARIO = 1419702340U;
+        static const AkUniqueID PLAYER_CAUTIONALARM = 4240862283U;
+        static const AkUniqueID PLAYER_MOVEMENT = 541470702U;
+        static const AkUniqueID UI_BUTTON_BACK = 1598004116U;
+        static const AkUniqueID UI_BUTTON_CLICK = 1664509575U;
+        static const AkUniqueID UI_BUTTON_DENIED = 1098067136U;
+        static const AkUniqueID UI_BUTTON_STARTGAME = 916601097U;
+        static const AkUniqueID UI_MAINTENANCE_CLOSE = 1676561250U;
+        static const AkUniqueID UI_MAINTENANCE_OPEN = 701705398U;
+        static const AkUniqueID UI_MAINTENANCE_POWERUP = 120049226U;
+        static const AkUniqueID UI_MAINTENANCE_REPAIR = 26115229U;
+        static const AkUniqueID UI_MISSION_TARGETPOPUP = 2991794566U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PLAYERHEALTHSWITCH
+        {
+            static const AkUniqueID GROUP = 206614296U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PLAYERHEALTH20 = 1703797114U;
+                static const AkUniqueID PLAYERHEALTH50 = 1619908955U;
+                static const AkUniqueID PLAYERHEALTH100 = 3528909237U;
+            } // namespace SWITCH
+        } // namespace PLAYERHEALTHSWITCH
+
+        namespace SURFACETYPE
+        {
+            static const AkUniqueID GROUP = 63790334U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID FOLIAGE = 1162469934U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace SWITCH
+        } // namespace SURFACETYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CAMERADISTANCE = 2835450985U;
+        static const AkUniqueID MENUATTENUATION = 466717960U;
+        static const AkUniqueID PLAYERHEALTHVALUE = 2774557527U;
+        static const AkUniqueID PLAYERMOVEMENTSPEED = 2557281390U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ELCHAPO = 3768109323U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID ENEMIES = 2242381963U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID NON_WORLD = 3869140995U;
+        static const AkUniqueID OBJECTS = 1695690031U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID WORLD = 2609808943U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
